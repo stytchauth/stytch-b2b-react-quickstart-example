8b0c84100f242774385b3892565a328ef788eeb3
Lydia Gorham <lydia@stytch.com>
2024-09-12 11:47:15 -0700

    Add Role Copy Override to Settings & SSO

--- src/components/SSO.tsx
+++ src/components/SSO.tsx
@@ -4,7 +4,28 @@ const SSO = () => {
 
   const styles = { fontFamily: 'Courier New' };
 
-  return <AdminPortalSSO styles={styles} />;
+  const config = {
+    getRoleDescription: (role: any) => {
+      if (role.role_id == 'stytch_admin') {
+          return 'Able to manage settings and members'
+      } else if (role.role_id == 'stytch_member') {
+          return 'Able to view settings and members, but cannot edit'
+      } else {
+          return 'Your custom role description here!'
+      }
+  },
+  getRoleDisplayName: (role: any) => {
+      if (role.role_id == 'stytch_admin') {
+          return 'Admin'
+      } else if (role.role_id == 'stytch_member') {
+          return 'Member'
+      } else {
+          return role.role_id
+      }
+  }
+  }
+
+  return <AdminPortalSSO config={config} styles={styles} />;
 };
 
 export default SSO;
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -6,6 +6,24 @@ const Settings = () => {
 
   const config = {
     allowedAuthMethods: [AdminPortalB2BProducts.emailMagicLinks, AdminPortalB2BProducts.sso],
+    getRoleDescription: (role: any) => {
+      if (role.role_id == 'stytch_admin') {
+          return 'Able to manage settings and members'
+      } else if (role.role_id == 'stytch_member') {
+          return 'Able to view settings and members, but cannot edit'
+      } else {
+          return 'Your custom role description here!'
+      }
+  },
+  getRoleDisplayName: (role: any) => {
+      if (role.role_id == 'stytch_admin') {
+          return 'Admin'
+      } else if (role.role_id == 'stytch_member') {
+          return 'Member'
+      } else {
+          return role.role_id
+      }
+  }
   };
 
   return <AdminPortalOrgSettings styles={styles} config={config} />;
