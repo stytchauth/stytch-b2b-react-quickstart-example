889cb11ddedf5308d48517319b1f60f87347ff7b
Lydia Gorham <lydia@stytch.com>
2024-09-13 11:02:05 -0700

    shared utils for admin portal config and styles

--- src/components/SSO.tsx
+++ src/components/SSO.tsx
@@ -1,31 +1,8 @@
 import { AdminPortalSSO } from '@stytch/react/b2b/adminPortal';
+import { config, styles } from '../utils/adminPortalConfig';
 
 const SSO = () => {
-
-  const styles = { fontFamily: 'Courier New' };
-
-  const config = {
-    getRoleDescription: (role: any) => {
-      if (role.role_id == 'stytch_admin') {
-          return 'Able to manage settings and members'
-      } else if (role.role_id == 'stytch_member') {
-          return 'Able to view settings and members, but cannot edit'
-      } else {
-          return 'Your custom role description here!'
-      }
-  },
-  getRoleDisplayName: (role: any) => {
-      if (role.role_id == 'stytch_admin') {
-          return 'Admin'
-      } else if (role.role_id == 'stytch_member') {
-          return 'Member'
-      } else {
-          return role.role_id
-      }
-  }
-  }
-
-  return <AdminPortalSSO config={config} styles={styles} />;
+    return <AdminPortalSSO config={config} styles={styles} />;
 };
 
 export default SSO;
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,32 +1,8 @@
-import { AdminPortalOrgSettings, AdminPortalB2BProducts } from '@stytch/react/b2b/adminPortal';
+import { AdminPortalOrgSettings } from '@stytch/react/b2b/adminPortal';
+import { config, styles } from '../utils/adminPortalConfig';
 
 const Settings = () => {
-
-  const styles = { fontFamily: 'Courier New' };
-
-  const config = {
-    allowedAuthMethods: [AdminPortalB2BProducts.emailMagicLinks, AdminPortalB2BProducts.sso],
-    getRoleDescription: (role: any) => {
-      if (role.role_id == 'stytch_admin') {
-          return 'Able to manage settings and members'
-      } else if (role.role_id == 'stytch_member') {
-          return 'Able to view settings and members, but cannot edit'
-      } else {
-          return 'Your custom role description here!'
-      }
-  },
-  getRoleDisplayName: (role: any) => {
-      if (role.role_id == 'stytch_admin') {
-          return 'Admin'
-      } else if (role.role_id == 'stytch_member') {
-          return 'Member'
-      } else {
-          return role.role_id
-      }
-  }
-  };
-
-  return <AdminPortalOrgSettings styles={styles} config={config} />;
+    return <AdminPortalOrgSettings styles={styles} config={config} />;
 };
 
 export default Settings;
