1d504c6393822f612b558a19971ee571cce05b58
Lydia Gorham <lydia@stytch.com>
2025-05-16 10:11:25 -0700

    Use Callback to Display TOTP Recovery Codes

--- src/components/LoginOrSignup.tsx
+++ src/components/LoginOrSignup.tsx
@@ -1,11 +1,24 @@
 import { StytchB2B } from "@stytch/react/b2b";
 import { discoveryConfig, discoveryStyles } from '../utils/stytchConfig';
+import { StytchEventType } from '@stytch/vanilla-js';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginOrSignup = () => {
+  const navigate = useNavigate();
 
   return (
     <div className="centered-login">
-      <StytchB2B config={discoveryConfig} styles={discoveryStyles} />
+      <StytchB2B
+      config={discoveryConfig}
+      styles={discoveryStyles}
+      callbacks={{
+        onEvent: (event) => {
+          if (event.type === StytchEventType.AuthenticateFlowComplete) {
+            navigate('/dashboard', { replace: true });
+          }
+        },
+      }}
+      />
     </div>
   );
 };
